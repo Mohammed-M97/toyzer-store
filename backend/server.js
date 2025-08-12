9d626ef0db6fe4b66f4579f67dc808fbac0ba1cb
Mohammed-M97 <mohammed-alawlaqi@outlook.com>
2025-08-12 23:58:29 +0300

    Project completed

--- backend/server.js
+++ backend/server.js
@@ -16,13 +16,16 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirnamelang = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json({ limit: "10mb" })); // for parsing application/json
 app.use(cookieParser());
-app.use('/locales', express.static(path.join(__dirname, 'locales')));
+app.use('/locales', express.static(path.join(__dirnamelang, 'locales')));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/products', prodectRoutes);
@@ -30,6 +33,14 @@ app.use('/api/cart', cartRoutes);
 app.use('/api/coupons', couponRoutes);
 app.use('/api/payments', paymentsRoutes);
 app.use('/api/analytics', analyticsRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+    
+}
 
 app.listen(PORT, () => {
     console.log('Server is running on http://localhost:' + PORT);
